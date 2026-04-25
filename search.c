7c2127c22ad9c883bd5ee22b9947906e1b323ca2
Mandeep Singh <mandeep.singh.uk07@gmail.com>
2026-04-26 01:03:57 +0530

    made the main searching process for files

---
+++ search.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include<dirent.h>
+#include<sys/stat.h>
+#include<string.h>
+#include<stdbool.h>
+
+long long full_size(const char *path);
+
+int list_files(const char *path)
+{
+    DIR *directory;
+    struct dirent *entry;
+    struct stat filestatus;
+    directory= opendir(path);
+    char full_path[1024];
+
+    if(directory==NULL)
+    {
+        printf("wrong paath has been entred");
+        closedir(directory);
+        return 1;
+    }
+    
+    while((entry=readdir(directory))!=0)
+    {
+        if(strcmp(entry->d_name,".")==0||strcmp(entry->d_name,"..")==0)
+        {
+            continue;
+        }
+
+        snprintf(full_path,sizeof(full_path),"%s/%s",path,entry->d_name);
+
+        if(stat(full_path,&filestatus)==0)
+        {
+            if(S_ISDIR(filestatus.st_mode))
+            {
+                printf("FOLDER: %s ,size: %lld \n",entry->d_name,(full_size(full_path)/((long long)1024)));
+            }
+            else
+            {
+                printf("FILE: %s ,size:%lld \n",entry->d_name,((long long)filestatus.st_size)/((long long)1024));
+            }
+        }
+    }
+    closedir(directory);
+    return 0;
+}
+long long full_size(const char *path){
+    DIR *directory;
+    struct dirent *entry;
+    struct stat filestatus;
+    char full_path[1024];
+    directory=opendir(path);
+    int total_length=0;
+
+    while((entry=readdir(directory))!=0)
+    {
+        if(strcmp(entry->d_name,".")==0||strcmp(entry->d_name,"..")==0)
+        {
+            continue;
+        }
+        snprintf(full_path,sizeof(full_path),"%s/%s",path,entry->d_name);
+        if(stat(full_path,&filestatus)==0)
+        {
+            if(S_ISDIR(filestatus.st_mode))
+            {
+                total_length+=full_size(full_path);
+            }
+            else
+            {
+                total_length+=filestatus.st_size;
+            }
+        }
+    }
+    closedir(directory);
+    return total_length;
+}
+
+
+int main(){
+    list_files(".");
+    return 0;
+}
